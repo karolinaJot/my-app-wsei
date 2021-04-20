b03100b3bcbd826c2741262ec52bfbc3c694f4e9
karolinaJot <amicus.karo@gmail.com>
2021-04-20 14:25:58 +0200

    current user card styles refactoring

--- src/components/leftBar/LeftBar.tsx
+++ src/components/leftBar/LeftBar.tsx
@@ -6,91 +6,98 @@ import { FontSize } from '../../styledHelpers/FontSizes';
 
 
 const Wrapper = styled.aside`
-    height: 370px;
     flex: 2;
     margin-right: 40px;
 `
-const CurrentUser = styled.div`
+const CurrentUserCard = styled.div`
     height: 225px;
-    /* width: 100%; */
     padding: 18px 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
     background-color: ${Colors.white};
     border-radius: 3%;
     box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
 `
+const UserCardInfoWrapper = styled.div`
+    border-bottom: solid 3px ${Colors.bg};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
 
 const ImageBox = styled.div`
     width: 70px;
     height: 70px;
     border-radius: 50%;
     overflow: hidden;
-`
-const UserImage = styled.img`
-    
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    };
 `
 
-const UserInfo = styled.div`
-
-`
-const Name = styled.p`
+const Name = styled.span`
     color: #5A6AB3;
     padding-top:18px;
     text-align: center;
     font-size: ${FontSize[18]};
-`
-const Job = styled.p`
+`;
+const Job = styled.span`
     padding: 18px 0;
-    border-bottom: solid 3px ${Colors.bg};
     text-align: center;
     font-size: ${FontSize[16]};
     color:  #B6B9C2;
-
-`
-const UserNetworkPublications = styled.div`
+`;
+const CradLinksWrapper = styled.div`
     display: flex;
     flex-direction: row;
     padding-top: 15px;
-    
+`;
 
-`
-const NetworkPublicationsText = styled.p`
-    flex: 4;
-    text-align: center;
+const CardLinksItem = styled.li`
+    display: flex;
 
-`
+    span {
+        padding-left: 10px;
+    };
+`;
 
-const CurrentUserIcons = styled.img`
-    text-align: center;
-    flex: 0;
+const CardLinksItemIconBox = styled.span`
 
-`
+    img {
+
+    };
+`;
+
+const CardLinksItemText = styled.span`
+`;
+
+const CardLinksItemBtn = styled.span`
+
+    button {
+        width: 35px;
+        height: 25px;
+        background-repeat: no-repeat;
+        background-size: 16px;
+        background-position: center;
+        background-color: ${Colors.white};
+        border: solid 1px ${Colors.black};
+        border-radius: 10%;
+        box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.6);
+    };
+`;
+
+
 const PersonButton = styled.button`
     background-image: url('./media/icons/people.png');
-    width: 35px;
-    height: 25px;
-    background-repeat: no-repeat;
-    background-size: 16px;
-    background-position: center;
-    background-color: ${Colors.white};
-    border: solid 1px ${Colors.black};
-    border-radius: 10%;
-    box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.6);
-`
+`;
+
 const PlusButton = styled.button`
     background-image: url('./media/icons/plus.png');
-    width: 35px;
-    height: 25px;
-    background-repeat: no-repeat;
-    background-size: 16px;
-    background-position: center;
-    background-color: ${Colors.white};
-    border: solid 1px ${Colors.black};
-    border-radius: 10%;
-    box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.6);
-`
+`;
+
+
 const LinksListWrapper = styled.div`
 
 `;
@@ -108,7 +115,7 @@ const ListItem = styled.li`
 
     span {
         padding-left: 10px;
-    }
+    };
 `;
 
 const IconBox = styled.span`
@@ -119,25 +126,37 @@ const IconBox = styled.span`
 const LeftBar: FC = () => {
     return (
         <Wrapper>
-            <CurrentUser>
-                <ImageBox>
-                    <UserImage src='./media/icons/userAvatar_2.jpg'></UserImage>
-                </ImageBox>
-                <UserInfo>
+            <CurrentUserCard>
+                <UserCardInfoWrapper>
+                    <ImageBox>
+                        <img src='./media/icons/userAvatar_2.jpg'></img>
+                    </ImageBox>
                     <Name>Aniela Kowalska</Name>
                     <Job>Job title - Company</Job>
-                    <UserNetworkPublications>
-                        <CurrentUserIcons src='./media/icons/network.png'></CurrentUserIcons>
-                        <NetworkPublicationsText>Your Network</NetworkPublicationsText>
-                        <PersonButton></PersonButton>
-                    </UserNetworkPublications>
-                    <UserNetworkPublications>
-                        <CurrentUserIcons src='./media/icons/publications.png'></CurrentUserIcons>
-                        <NetworkPublicationsText>Your Publications</NetworkPublicationsText>
-                        <PlusButton></PlusButton>
-                    </UserNetworkPublications>
-                </UserInfo>
-            </CurrentUser>
+                </UserCardInfoWrapper>
+                <CradLinksWrapper>
+                    <ul>
+                        <CardLinksItem>
+                            <CardLinksItemIconBox>
+                                <img src='./media/icons/network.png'></img>
+                            </CardLinksItemIconBox>
+                            <CardLinksItemText>Your Network</CardLinksItemText>
+                            <CardLinksItemBtn>
+                                <PersonButton></PersonButton>
+                            </CardLinksItemBtn>
+                        </CardLinksItem>
+                        <CardLinksItem>
+                            <CardLinksItemIconBox>
+                                <img src='./media/icons/publications.png'></img>
+                            </CardLinksItemIconBox>
+                            <CardLinksItemText>Your Publications</CardLinksItemText>
+                            <CardLinksItemBtn>
+                                <PlusButton></PlusButton>
+                            </CardLinksItemBtn>
+                        </CardLinksItem>
+                    </ul>
+                </CradLinksWrapper>
+            </CurrentUserCard>
             <LinksListWrapper>
                 <ul>
                     <ListItem>
