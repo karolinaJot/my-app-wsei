396c9aaa51ad738db840d136057ee3fcc1de6bb6
karolinaJot <amicus.karo@gmail.com>
2021-04-25 22:07:18 +0200

    add UnderConstructionPage

---
+++ src/components/underConstructionPage/UnderConstructionPage.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+
+
+const Wrapper = styled.div``;
+
+export const UnderConstructionPage: FC = () => {
+
+    return(
+        <Wrapper>
+
+        </Wrapper>
+
+    );
+};
