b31f85cbe3d6b3990e902a727d4ed68535f8b435
karolinaJot <amicus.karo@gmail.com>
2021-05-05 14:10:33 +0200

    add Resume elements

---
+++ src/components/mainContent/resume/ResumeHeader.tsx
@@ -0,0 +1,55 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+import { Colors } from '../../../styledHelpers/Colors';
+import { FontSize } from '../../../styledHelpers/FontSizes';
+
+
+const Wrapper = styled.div`
+    margin-top: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    span {
+        display: block;
+    };
+`;
+
+const HeaderText = styled.span`
+    font-size: ${FontSize[18]};
+    font-weight: 700;
+    display: block;
+    color: grey;
+    margin-left: 10px;
+`;
+
+const RightSideWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const FilerBox = styled.span`
+    color: lightgray;
+    margin-right: 50px;
+`;
+
+const FollowBox = styled.span`
+    color: #0381BE;
+    margin-right: 10px;
+`;
+
+
+const ResumeHeader: FC = () =>{
+    return(
+        <Wrapper>
+            <HeaderText>Resume your work</HeaderText>
+            <RightSideWrapper>
+                <FilerBox>Filter by title...</FilerBox>
+                <FollowBox>O Followed</FollowBox>
+            </RightSideWrapper>
+        </Wrapper>
+    );
+};
+
+export default ResumeHeader;
---
+++ src/components/mainContent/resume/ResumeItem.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+import { Colors } from '../../../styledHelpers/Colors';
+
+
+const ResumeItem: FC = () =>{
+    return(
+        <></>
+    );
+};
+
+export default ResumeItem;
