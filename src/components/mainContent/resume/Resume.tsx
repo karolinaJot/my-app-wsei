58d6ffbad9319aa4e9bc79f022eb5cb9f3d441df
karolinaJot <amicus.karo@gmail.com>
2021-06-08 13:08:51 +0200

    add pagination to main page

--- src/components/mainContent/resume/Resume.tsx
+++ src/components/mainContent/resume/Resume.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent, FC, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { IState } from '../../../reducers';
@@ -9,7 +10,7 @@ import { IUsersReducer } from '../../../reducers/usersReducers';
 import { Colors } from '../../../styledHelpers/Colors';
 import ResumeHeader from './ResumeHeader';
 import ResumeItem from './ResumeItem';
-import ResumeNavigation from './ResumeNavigation';
+
 
 
 
@@ -23,6 +24,22 @@ const Wrapper = styled.div`
 const ResumeItemsWrapper = styled.div`
  `;
 
+const ResumeNavigation = styled.div`
+   padding-bottom: 20px;
+   display: flex;
+   text-align: center;
+
+    ul {
+        margin: 0 auto;
+        display: flex;
+    }
+    
+    ul li {
+        padding-right: 10px;
+        cursor: pointer;
+    }
+`;
+
 export const Resume: FC = () => {
 
     const { usersList, photosList, commentsList } = useSelector<IState, IUsersReducer & IPhotosReducer & ICommetsReducer>(state => ({
@@ -33,33 +50,50 @@ export const Resume: FC = () => {
     }));
 
     const [inputText, setInputText] = useState<string>("");
+    const [currentPage, setCurentPage] = useState<number>(0);
 
     const inputHandler = (e: ChangeEvent<HTMLInputElement>) => {
         const text = e.target.value;
         setInputText(text);
     };
 
-
+    const handlePageClick = (data: any) => {
+        const selected = data.selected;
+        setCurentPage(selected);
+    }
 
     return (
         <Wrapper>
             <ResumeHeader changeText={inputHandler} />
             {commentsList &&
                 <ResumeItemsWrapper>
-                    {
-                        commentsList.map(comment => 
+                    {commentsList.slice(currentPage, currentPage + 10)
+                        .map(comment =>
                             <ResumeItem
                                 key={comment.id}
                                 title={comment.name}
                                 body={comment.body}
-                                author={usersList[comment.postId -1]? usersList[comment.postId -1].name : usersList[0].name  }
-                                company={usersList[comment.postId - 1]? usersList[comment.postId -1].company.name : usersList[0].company.name }
+                                author={usersList[comment.postId - 1] ? usersList[comment.postId - 1].name : usersList[0].name}
+                                company={usersList[comment.postId - 1] ? usersList[comment.postId - 1].company.name : usersList[0].company.name}
                             />
                         )
                     }
                 </ResumeItemsWrapper>
             }
-            <ResumeNavigation></ResumeNavigation>
+            <ResumeNavigation>
+                <ReactPaginate
+                    previousLabel={'previous'}
+                    nextLabel={'next'}
+                    breakLabel={'...'}
+                    breakClassName={'break-me'}
+                    pageCount={commentsList.length / 10}
+                    marginPagesDisplayed={3}
+                    pageRangeDisplayed={3}
+                    onPageChange={handlePageClick}
+                    containerClassName={'pagination'}
+                    activeClassName={'active'}
+                />
+            </ResumeNavigation>
         </Wrapper>
     );
 
