58974159793e6c9a5f7f6afb735b4f4166ee9775
karolinaJot <amicus.karo@gmail.com>
2021-06-13 11:22:48 +0200

    add Card component

--- src/components/workspacePage/WorkspaceCenter.tsx
+++ src/components/workspacePage/WorkspaceCenter.tsx
@@ -1,15 +1,55 @@
 import { FC } from 'react';
 import styled from 'styled-components';
+import { Colors } from '../../styledHelpers/Colors';
+import { FontSize } from '../../styledHelpers/FontSizes';
+import WorkspaceCenterCard from './WorkspaceCenterCard';
 
 
 const Wrapper = styled.div`
+    border: 2px solid green;
+    width: 100%;
+    background-color: #c0c0c0;
+    border-radius: 5px;
+    box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.4);
+    margin-top: 20px;
 
 `;
 
+const HeaderWrapper = styled.div`
+    margin: 10px 20px 20px 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    font-size: ${FontSize[16]};
+    font-weight: 400;
+    color: ${Colors.black};
+`;
+
+const CardWrapper = styled.div`
+    margin: 0 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+
+
+
+
 const WorkspaceCenter: FC = () => {
     return (
-        <Wrapper>Worspace center</Wrapper>
+        <Wrapper>
+            <HeaderWrapper>
+                <span>Start working on corporate matters</span>
+                <span>Hide</span>
+            </HeaderWrapper>
+            <CardWrapper>
+                <WorkspaceCenterCard />
+                <WorkspaceCenterCard />
+                <WorkspaceCenterCard />
+            </CardWrapper>
+        </Wrapper>
     );
 }
- 
+
 export default WorkspaceCenter;
